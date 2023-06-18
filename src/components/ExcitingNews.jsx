1cd92124ece7e3cc0617b2e71f70eb17b2de64bb
belem <graceitamunoala@gmail.com>
2023-06-18 23:31:59 +0100

    setting image sets for screen sizes

--- src/components/ExcitingNews.jsx
+++ src/components/ExcitingNews.jsx
@@ -41,7 +41,7 @@ const ExcitingNews = () => {
             <div className=''>
                 <p className='text-2xl md:text-4xl text-red-700 font-bold my-6'>EXCITING NEWS</p>
             </div>
-            <div className='border w-3/4 border-yellow-600 border-y-2'></div>
+            <div className='border w-1/2 md:w-3/4 border-yellow-600 border-y-2'></div>
 
             <div className='w-full h-full flex-row items-center justify-center'>
                 <ul className='flex flex-row mx-20 my-8'>
@@ -51,12 +51,21 @@ const ExcitingNews = () => {
                     >
                         <RiArrowLeftSLine size={40} className='text-red-500'/>
                     </button>
+
+                    {/* set image set - for md screens */}
                     {slicedImages.map(() => (
-                        <li className='mx-4 mb-10'>
+                        <li className='hidden md:flex mx-4 mb-10'>
                             <img src={images[currentImageIndex]} className='transition-all duration-300 
                             ease-in-out filter brightness-100 hover:brightness-50 hover:scale-105'/>
                         </li>
                     ))}
+
+                    {/* set image set for sm screens */}
+                    <img
+                        className="md:hidden sm:flex w-3/4 mb-10 flex items-center justify-between rounded-2xl shadow-2xl"
+                        src={images[currentImageIndex]}
+                        alt="Slideshow Image"
+                    />    
                     <button
                         className="flex justify-between mt-16"
                         onClick={goToNextImage}
@@ -64,6 +73,7 @@ const ExcitingNews = () => {
                         <MdOutlineKeyboardArrowRight size={40} className='text-red-500' />
                     </button>
                 </ul>
+
             </div>
         </div>
         // comment
