32cec465e105c8a397abe6648ce1b90a204e5f4d
belem <graceitamunoala@gmail.com>
2023-06-18 23:42:49 +0100

    sm responsiveness

--- src/components/ExcitingNews.jsx
+++ src/components/ExcitingNews.jsx
@@ -39,14 +39,14 @@ const ExcitingNews = () => {
     return (
         <div className='flex flex-col items-center justify-between bg-white'>
             <div className=''>
-                <p className='text-2xl md:text-4xl text-red-700 font-bold my-6'>EXCITING NEWS</p>
+                <p className='text-3xl md:text-4xl text-red-700 font-bold mt-5 mb-4 md:my-6'>EXCITING NEWS</p>
             </div>
-            <div className='border w-1/2 md:w-3/4 border-yellow-600 border-y-2'></div>
+            <div className='border w-3/4 border-yellow-600 border-y-2'></div>
 
             <div className='w-full h-full flex-row items-center justify-center'>
-                <ul className='flex flex-row mx-20 my-8'>
+                <ul className='flex flex-row mx-3 md:mx-20 my-8'>
                     <button
-                        className="flex justify-between mt-16"
+                        className="flex justify-between mt-24 md:mt-16"
                         onClick={goToPreviousImage}
                     >
                         <RiArrowLeftSLine size={40} className='text-red-500'/>
@@ -62,12 +62,13 @@ const ExcitingNews = () => {
 
                     {/* set image set for sm screens */}
                     <img
-                        className="md:hidden sm:flex w-3/4 mb-10 flex items-center justify-between rounded-2xl shadow-2xl"
+                        className="md:hidden sm:flex w-80 mb-5 flex items-center justify-center transition-all duration-300 
+                        ease-in-out filter brightness-100 hover:brightness-50 hover:scale-105"
                         src={images[currentImageIndex]}
                         alt="Slideshow Image"
                     />    
                     <button
-                        className="flex justify-between mt-16"
+                        className="flex justify-between mt-24 md:mt-16"
                         onClick={goToNextImage}
                     >
                         <MdOutlineKeyboardArrowRight size={40} className='text-red-500' />
