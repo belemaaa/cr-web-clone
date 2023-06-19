05eee26b8473e3603610f1f4666e3afb1690a062
belem <graceitamunoala@gmail.com>
2023-06-19 17:27:03 +0100

    props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import slide1 from './assets/proj/slide1.jpg'
+import slide2 from './assets/proj/slide2.jpg'
+import slide3 from './assets/proj/slide3.jpg'
+import slide4 from './assets/proj/slide4.jpg'
+import slide5 from './assets/proj/slide5.jpg'
+import slide6 from './assets/proj/slide6.jpg'
+import everyday1 from './assets/proj/everyday1.jpg'
+import everyday2 from './assets/proj/everyday2.jpg'
+import everyday3 from './assets/proj/everyday3.jpg'
+import everyday4 from './assets/proj/everyday4.jpg'
+import everyday5 from './assets/proj/everyday5.jpg'
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import OrderNow from './components/OrderNow';
@@ -12,8 +23,15 @@ function App() {
       <NavBar/>
       <Home/>
       <OrderNow/>
-      <TasteTheLove/>
+      <TasteTheLove 
+        title='TASTE THE LOVE - NEW MEAL DEALS'
+        const images = {[slide1, slide2, slide3, slide4, slide5, slide6]}
+      />
       <ExcitingNews/>
+      <TasteTheLove
+        title='EVERYDAY AFFORDABLE VALUE MEALS'
+        const images = {[everyday1, everyday2, everyday3, everyday4, everyday5]}
+      />
     </div>
   );
 }
--- src/components/TasteTheLove.jsx
+++ src/components/TasteTheLove.jsx
@@ -8,9 +8,9 @@ import slide4 from '../assets/proj/slide4.jpg'
 import slide5 from '../assets/proj/slide5.jpg'
 import slide6 from '../assets/proj/slide6.jpg'
 
-const TasteTheLove = () => {
+const TasteTheLove = (props) => {
 
-    const images = [slide1, slide2, slide3, slide4, slide5, slide6]
+    const images = props.images
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
     // set functions to handle arrow clicks
@@ -34,7 +34,7 @@ const TasteTheLove = () => {
     return (
         <div className='bg-yellow-500 flex flex-col items-center justify-between'>
             <div>
-                <p className='text-red-600 pt-8 md:pt-10 pb-7 text-xl lg:text-4xl font-bold'>TASTE THE LOVE - NEW MEAL DEALS</p>
+                <p className='text-red-600 pt-8 md:pt-10 pb-7 text-xl lg:text-4xl font-bold'>{props.title}</p>
             </div>
 
             <div className="w-full h-full flex flex-row items-center justify-center">
