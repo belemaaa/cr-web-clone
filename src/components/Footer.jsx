d32a5ccb9604238fddbaafb92e058e6989701138
belem <graceitamunoala@gmail.com>
2023-06-20 12:50:18 +0100

    committed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-red-600'>Footer</div>
+    <div className='bg-red-600 flex items-center justify-center'>
+            <p className='text-white my-3 text-xs md:text-sm'>
+                Copyright 2022 <span className='font-bold'>Chicken Republic</span>, All Rights Reserved.
+            </p>
+    </div>
   )
 }
 
